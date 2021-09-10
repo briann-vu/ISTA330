945b5153435bbfab462cf930c222581beef5709a
Brian <brianvu@email.arizona.edu>
2021-09-09 23:44:18 -0700

    q3

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -11,8 +11,8 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9), (6, 9)
 var numberOfOrderedPairs = function(input) {
     var count = 0;
     for (var x = 0; x < input.length; x++) {
-        for (var y of input) {
-            if (input[x] < y) {
+        for (var y = x; y < input.length; y++) {
+            if (input[x] < input[y]) {
                 count++
             }
         }
