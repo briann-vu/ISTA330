ddaf3c34857023ea7436779148b2cbac92393e33
Brian <brianvu@email.arizona.edu>
2021-09-09 23:58:34 -0700

    q5

--- practicum/week-2/question5.js
+++ practicum/week-2/question5.js
@@ -12,6 +12,17 @@ output: 3
 */
 
 var minimalReduction = function(n) {
-
+    var ops = 0
+    while (n != 0) {
+        if (n % 4 == 0) {
+            n = n /4
+        } else if (n % 3 == 0) {
+            n = n / 3
+        } else {
+            n--
+        }
+        ops++
+    }
+    return ops
 };
 
