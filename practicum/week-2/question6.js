93b8ef6c7106b1c6e1d14e7ab78af2596063dd07
Brian <brianvu@email.arizona.edu>
2021-09-10 00:07:50 -0700

    q6

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -6,5 +6,12 @@ output: 3
 */
 
 var median = function(input) {
-
+    if (input.length % 2 == 0) {
+        var midIndex1 = input.length / 2
+        var midIndex2 = midIndex1 - 1
+        return ((input[midIndex1] + input[midIndex2]) / 2)
+    } else {
+        var midIndex = (input.length - 1) / 2
+        return input[midIndex]
+    }
 };
