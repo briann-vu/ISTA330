78bea9d93fd963331fb2173b7df1d140af078025
Brian <brianvu@email.arizona.edu>
2021-09-10 00:39:54 -0700

    q10

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,17 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    var num = n.toString()
+    numbers = []
+    for (x=0;x<num.length;x++) {
+        var integer = parseInt(x, 10);
+        numbers.push(integer)
+    }
+    var maxNum = 0
+    for (var i of numbers) {
+        if (i > maxNum) {
+            maxNum = i
+        }
+    }
+    return maxNum
 };
