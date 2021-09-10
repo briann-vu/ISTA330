22553de70f78ffb5efd09a6d2a1ceff4e183b41c
Brian <brianvu@email.arizona.edu>
2021-09-10 03:15:03 -0700

    q2

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -18,8 +18,14 @@ var canGetEqualCookies = function(cookies, extraCookies) {
                         extraCookies -= temp
                 }
         }
-        if (extraCookies < 0 || extraCookies > 0) {
-                return false
+                if (extraCookies > 0) {
+                        for (var cookie of cookies) {
+                                cookie++
+                                extraCookies--
+                        }
+                }
+        if (extraCookies < 0) {
+        return false
         } 
         return true
-}       ;
+        };
