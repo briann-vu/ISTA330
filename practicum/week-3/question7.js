297c9a4b48e2c0b1a392a9b578d35189e46b5bfa
Brian <brianvu@email.arizona.edu>
2021-09-15 15:34:48 -0700

    q7

--- practicum/week-3/question7.js
+++ practicum/week-3/question7.js
@@ -6,11 +6,26 @@ new matrix whose rows are the columns
 of the original.
 
 Example: 
-input: [[1,2,3],[4,5,6]]
-output: [[1,4], [2,5], [3,6]]
+input: [[1,2,3],
+        [4,5,6]]
+
+output: [[1,4], 
+         [2,5], 
+         [3,6]]
 
 */
 
 var transpose = function(M) {
-
+    var ans = []
+    var x = 0
+    for (var i = 0; i <= M.length; i++) {
+        var helper = []
+        for (var m of M) {
+            helper.push(m[x])
+        }
+        x++
+        ans.push(helper)
+    }
+	return ans
+    
 };
