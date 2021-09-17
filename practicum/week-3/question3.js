b80c6e371ad89568ec1ea73221a959efc77fb5b7
Brian <brianvu@email.arizona.edu>
2021-09-17 00:37:07 -0700

    q3

--- practicum/week-3/question3.js
+++ practicum/week-3/question3.js
@@ -13,5 +13,25 @@ output: [
 */
 
 var PascalTriangle = function(n) {
-
+  if (n == 1) {
+    return [1]
+  } else if (n == 2) {
+    return [[1], [1,1]]
+  }
+  var answer = [[1], [1,1]]
+  var prev = [1,1]
+  for (var x = 0; x < n - 2; x ++) {
+    var arrToAdd = [1]
+    for (var i = 0; i < prev.length-1; i++) {
+      arrToAdd.push(prev[i] + prev[i+1])
+    }
+    arrToAdd.push(1)
+    prev = arrToAdd
+    answer.push(arrToAdd)
+  }
+	return answer
 };
+
+
+
+
